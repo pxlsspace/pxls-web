dc74ee6365f207f5c29282cc73b10a2b6d90beb5
Vanilla Black <vanilla@f66.dev>
2023-12-07 13:50:50 -0500

    Fix ws connect bug and clean up language handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const http = require('http');
 const PO = require('pofile');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { create } = require('express-handlebars');
@@ -7,6 +8,7 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 
 const app = express();
+const server = http.createServer(app);
 const hbs = create({
   partialsDir: path.join(__dirname, 'views', 'partials')
 });
@@ -60,50 +62,19 @@ const wsProxy = createProxyMiddleware({
   target: 'ws://localhost:4567/ws',
   changeOrigin: true,
   ws: true,
-  logLevel: 'error',
-  onProxyReq: function (proxyReq, req, res) {
-    res.send();
-  }
+  logLevel: 'silent'
 });
 
-const loadPO = (path) => new Promise((resolve, reject) => {
-  PO.load(path, (error, pofile) => {
-    if (error) {
-      reject(error);
-    } else {
-      resolve(pofile);
-    }
-  });
-});
-
-function i18n(str, poFile, args) {
-  const item = poFile.items.find(i => i.msgid === str);
-  if (!item) {
-    console.warn(`No translation found for ${str}`);
-  }
-  str = (item && item.msgstr[0]) || str;
-  for (let i = 0; i < args.length; i++) {
-    str = str.replace(`{${i}}`, args[i]);
-  }
-  return str;
-}
+server.on('upgrade', wsProxy.upgrade);
 
 app.use('/', async (req, res, next) => {
   const fileName = req.path.slice(1);
   const filePath = path.join(__dirname, 'dist', fileName);
-  const langCode = req.cookies['pxls-accept-language-override'] || 'en';
-  let poFile;
-  try {
-    poFile = await loadPO(path.join(__dirname, 'po', `Localization${langCode === 'en' ? '' : '_' + langCode}.po`));
-  } catch (e) {
-    console.error(e);
-    res.status(500).send('Internal Server Error');
-    return;
-  }
+  const { poFile, langCode } = await getLanguageData(req, res);
 
   const proxyFetch = (url, options = {}) => fetch(url, {
-    headers: req.headers,
-    ...options
+    ...options,
+    headers: req.headers
   });
 
   if (fileName === '') {
@@ -113,9 +84,7 @@ app.use('/', async (req, res, next) => {
       head: '',
       scriptLang: langCode === 'en' ? '' : '_' + langCode,
 
-      helpers: {
-        i18n: (str, ...args) => i18n(str, poFile, args)
-      }
+      helpers: handebarsHelpers(poFile)
     });
   } else if (/^profile$|profile\/(?!js|css)/.test(fileName)) {
     // get profile data
@@ -161,17 +130,9 @@ app.use('/', async (req, res, next) => {
 });
 
 async function sendErrorPage(req, res, code) {
-  const langCode = req.cookies['pxls-accept-language-override'] || 'en';
-  let poFile;
-  try {
-    poFile = await loadPO(path.join(__dirname, 'po', `Localization${langCode === 'en' ? '' : '_' + langCode}.po`));
-  } catch (e) {
-    console.error(e);
-    res.status(500).send('Internal Server Error');
-    return;
-  }
+  const { poFile } = await getLanguageData(req, res);
 
-  res.render('404', {
+  res.status(code).render('404', {
     palette: 'FFFFFF,C2CBD4,858D98,4B4F58,22272D,000000,38271D,6C422C,BC7541,FFB27F,FFD6BF,FEB2D9,F854CF,C785F3,9C29BC,562972,1E1E5B,153FA2,1C95DF,A0E8FF,17A8A3,226677,094C45,278242,43C91E,B7F954,FFFFAF,FAE70F,FEA815,EA5B15,5A0400,990700,D81515,FF635E',
     max_faction_tag_length: 5,
     max_faction_name_length: 20,
@@ -183,6 +144,45 @@ async function sendErrorPage(req, res, code) {
     helpers: handebarsHelpers(poFile)
   });
 }
+
+function i18n(str, poFile, args) {
+  if (typeof poFile.items === 'undefined') {
+    console.trace(str, poFile.poFile, args);
+    return;
+  }
+  const item = poFile.items.find(i => i.msgid === str);
+  if (!item) {
+    console.warn(`No translation found for ${str}`);
+  }
+  str = (item && item.msgstr[0]) || str;
+  for (let i = 0; i < args.length; i++) {
+    str = str.replace(`{${i}}`, args[i]);
+  }
+  return str;
+}
+
+async function getLanguageData(req, res) {
+  const langCode = req.cookies['pxls-accept-language-override'] || 'en';
+  let poFile;
+  try {
+    poFile = await loadPO(path.join(__dirname, 'po', `Localization${langCode === 'en' ? '' : '_' + langCode}.po`));
+  } catch (e) {
+    console.error(e);
+    await sendErrorPage(req, res, 500);
+    return;
+  }
+  return { poFile, langCode };
+}
+
+const loadPO = (path) => new Promise((resolve, reject) => {
+  PO.load(path, (error, poFile) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve(poFile);
+    }
+  });
+});
 
 function listFiles(dir, fileList = []) {
   const files = fs.readdirSync(dir);
@@ -199,4 +199,4 @@ function listFiles(dir, fileList = []) {
   return fileList;
 }
 
-app.listen(port, () => console.info('Listening at http://localhost:3000/'));
+server.listen(port, () => console.info('Listening at http://localhost:3000/'));
