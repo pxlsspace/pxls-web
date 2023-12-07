57f71b2ff48375fea4741051ea3bca2eddf66244
Vanilla Black <vanilla@f66.dev>
2023-12-06 23:13:20 -0500

    Conform to .eslintrc

--- gulpfile.js
+++ gulpfile.js
@@ -17,186 +17,185 @@ const { findTranslationCalls, contract } = require('./scripts/localization-util'
 const isDevEnvironment = process.env.NODE_ENV === 'dev';
 
 if (isDevEnvironment) {
-    console.info('❗ Development environment detected.');
+  console.info('❗ Development environment detected.');
 }
 
 function copy() {
-    return src([
-        'public/admin/**/*',
-        'public/themes/**/*',
-        'public/webfonts/**/*',
-        'public/*.wav',
-        // TODO: minify profile
-        'public/profile/**/*'], { base: 'public' })
-        .pipe(dest('dist'));
+  return src([
+    'public/admin/**/*',
+    'public/themes/**/*',
+    'public/webfonts/**/*',
+    'public/*.wav',
+    // TODO: minify profile
+    'public/profile/**/*'], { base: 'public' })
+    .pipe(dest('dist'));
 }
 
 function translateHTML(pofile) {
-    return through.obj(function (file, enc, callback) {
-        if (file.isBuffer()) {
-            PO.load(pofile, (error, pofile) => {
-                if (error) {
-                    callback(error);
-                } else {
-                    let contents = file.contents.toString();
-
-                    // replace all instances of {{i18n('Localization', '<localization>') | raw}} with the translation
-                    const regex = /{{i18n\('Localization', '(.+?)'\) \| raw}}/g;
-                    let match;
-                    while ((match = regex.exec(contents)) !== null) {
-                        let [original, localization] = match;
-
-                        localization = localization
-                            .replace(/\\'/g, "'");
-
-                        const item = pofile.items.find(i => i.msgid === localization);
-                        if (!item) {
-                            console.warn(`No translation found for ${localization}`);
-                        }
-
-                        const replace = (item && item.msgstr[0]) || original;
-
-                        contents = contents.replace(localization, replace);
-                    }
-
-                    file.contents = Buffer.from(contents);
-
-                    callback(null, file);
-                }
-            });
+  return through.obj(function (file, enc, callback) {
+    if (file.isBuffer()) {
+      PO.load(pofile, (error, pofile) => {
+        if (error) {
+          callback(error);
         } else {
-            callback(new Error('Expected buffer'));
+          let contents = file.contents.toString();
+
+          // replace all instances of {{i18n('Localization', '<localization>') | raw}} with the translation
+          const regex = /{{i18n\('Localization', '(.+?)'\) \| raw}}/g;
+          let match;
+          while ((match = regex.exec(contents)) !== null) {
+            let [original, localization] = match;
+
+            localization = localization
+              .replace(/\\'/g, '\'');
+
+            const item = pofile.items.find(i => i.msgid === localization);
+            if (!item) {
+              console.warn(`No translation found for ${localization}`);
+            }
+
+            const replace = (item && item.msgstr[0]) || original;
+
+            contents = contents.replace(localization, replace);
+          }
+
+          file.contents = Buffer.from(contents);
+
+          callback(null, file);
         }
-    })
+      });
+    } else {
+      callback(new Error('Expected buffer'));
+    }
+  });
 }
 
 function html() {
-    return src(['po/*.po'])
-        .pipe(through.obj(function (file, enc, callback) {
-            const codeIndex = file.basename.lastIndexOf('_');
-            const extIndex = file.basename.lastIndexOf('.');
-            const langcode = codeIndex === -1
-                ? ''
-                : file.basename.substring(codeIndex, extIndex === -1
-                    ? file.basename.length
-                    : extIndex);
+  return src(['po/*.po'])
+    .pipe(through.obj(function (file, enc, callback) {
+      const codeIndex = file.basename.lastIndexOf('_');
+      const extIndex = file.basename.lastIndexOf('.');
+      const langcode = codeIndex === -1
+        ? ''
+        : file.basename.substring(codeIndex, extIndex === -1
+          ? file.basename.length
+          : extIndex);
 
-            src(['public/**/*.html'], { read: true })
-                .pipe(buffer())
-                .pipe(translateHTML(file.path))
-                .pipe(rename({ suffix: langcode }))
-                .pipe(dest('dist'))
-                .once('end', callback);
-        }));
-
+      src(['public/**/*.html'], { read: true })
+        .pipe(buffer())
+        .pipe(translateHTML(file.path))
+        .pipe(rename({ suffix: langcode }))
+        .pipe(dest('dist'))
+        .once('end', callback);
+    }));
 }
 
 function css() {
-    return src('public/*.css')
-        .pipe(gulpIf(isDevEnvironment, sourcemaps.init()))
-        .pipe(cleanCSS())
-        .pipe(gulpIf(isDevEnvironment, sourcemaps.write()))
-        .pipe(dest('dist'));
+  return src('public/*.css')
+    .pipe(gulpIf(isDevEnvironment, sourcemaps.init()))
+    .pipe(cleanCSS())
+    .pipe(gulpIf(isDevEnvironment, sourcemaps.write()))
+    .pipe(dest('dist'));
 }
 
 function minJS() {
-    return src('public/*.min.js')
-        .pipe(dest('dist'));
+  return src('public/*.min.js')
+    .pipe(dest('dist'));
 }
 
 // NOTE ([  ]): pattern for all non-minified .js files
 const SOURCE_FILES = [
-    'public/**/*.js',
-    '!public/**/*.min.js',
-    '!public/**/*-min.js'
+  'public/**/*.js',
+  '!public/**/*.min.js',
+  '!public/**/*-min.js'
 ];
 
 function lint() {
-    return src(SOURCE_FILES)
-        .pipe(eslint())
-        .pipe(eslint.failAfterError());
+  return src(SOURCE_FILES)
+    .pipe(eslint())
+    .pipe(eslint.failAfterError());
 }
 
 function translate(pofile) {
-    return through.obj(function (file, enc, callback) {
-        if (file.isBuffer()) {
-            PO.load(pofile, (error, pofile) => {
-                if (error) {
-                    callback(error);
-                } else {
-                    let contents = file.contents.toString();
-                    const script = esprima.parseScript(contents, { range: true });
-                    const translationCalls = script.body
-                        .map(findTranslationCalls)
-                        .flat()
-                        .sort((a, b) => a.range.start - b.range.start);
-
-                    let offset = 0;
-
-                    for (const call of translationCalls) {
-                        const [argument] = call.arguments;
-
-                        const [start, end] = call.range;
-                        const length = end - start;
-
-                        const original = contract(contents.substring(...argument.range.map(p => p + offset)), 1);
-                        const quote = contents[argument.range[0] + offset];
-
-                        const item = pofile.items.find(i => i.msgid === original);
-
-                        const replaceContent = (item && item.msgstr[0]) || original;
-                        const replace = quote + replaceContent.replace(new RegExp(`([^\\\\])([${quote}])`, 'g'), '$1\\$2') + quote;
-
-                        // just in case something goes wrong
-                        if (contents.substring(start + offset, start + offset + 2) !== '__') {
-                            callback(new Error('Translation offset drift'));
-                        }
-
-                        contents = contents.substring(0, offset + start) + replace + contents.substring(offset + end);
-                        offset += replace.length - length;
-                    }
-
-                    file.contents = Buffer.from(contents);
-
-                    callback(null, file);
-                }
-            });
+  return through.obj(function (file, enc, callback) {
+    if (file.isBuffer()) {
+      PO.load(pofile, (error, pofile) => {
+        if (error) {
+          callback(error);
         } else {
-            callback(new Error('Expected buffer'));
+          let contents = file.contents.toString();
+          const script = esprima.parseScript(contents, { range: true });
+          const translationCalls = script.body
+            .map(findTranslationCalls)
+            .flat()
+            .sort((a, b) => a.range.start - b.range.start);
+
+          let offset = 0;
+
+          for (const call of translationCalls) {
+            const [argument] = call.arguments;
+
+            const [start, end] = call.range;
+            const length = end - start;
+
+            const original = contract(contents.substring(...argument.range.map(p => p + offset)), 1);
+            const quote = contents[argument.range[0] + offset];
+
+            const item = pofile.items.find(i => i.msgid === original);
+
+            const replaceContent = (item && item.msgstr[0]) || original;
+            const replace = quote + replaceContent.replace(new RegExp(`([^\\\\])([${quote}])`, 'g'), '$1\\$2') + quote;
+
+            // just in case something goes wrong
+            if (contents.substring(start + offset, start + offset + 2) !== '__') {
+              callback(new Error('Translation offset drift'));
+            }
+
+            contents = contents.substring(0, offset + start) + replace + contents.substring(offset + end);
+            offset += replace.length - length;
+          }
+
+          file.contents = Buffer.from(contents);
+
+          callback(null, file);
         }
-    });
+      });
+    } else {
+      callback(new Error('Expected buffer'));
+    }
+  });
 }
 
 function srcJS() {
-    return src(['po/*.po'])
-        .pipe(through.obj(function (file, enc, callback) {
-            const codeIndex = file.basename.lastIndexOf('_');
-            const extIndex = file.basename.lastIndexOf('.');
-            const langcode = codeIndex === -1
-                ? ''
-                : file.basename.substring(codeIndex, extIndex === -1
-                    ? file.basename.length
-                    : extIndex);
+  return src(['po/*.po'])
+    .pipe(through.obj(function (file, enc, callback) {
+      const codeIndex = file.basename.lastIndexOf('_');
+      const extIndex = file.basename.lastIndexOf('.');
+      const langcode = codeIndex === -1
+        ? ''
+        : file.basename.substring(codeIndex, extIndex === -1
+          ? file.basename.length
+          : extIndex);
 
-            src([...SOURCE_FILES, '!public/include/**/*.js', '!public/profile/**/*.js'], { read: false })
-                .pipe(tap(file => {
-                    file.contents = browserify(file.path, { debug: isDevEnvironment })
-                        .bundle();
-                }))
-                .pipe(buffer())
-                .pipe(translate(file.path))
-                .pipe(gulpIf(isDevEnvironment, sourcemaps.init({ loadMaps: true })))
-                .pipe(minify({
-                    ext: {
-                        src: '.src.js',
-                        min: '.js'
-                    }
-                }))
-                .pipe(gulpIf(isDevEnvironment, sourcemaps.write()))
-                .pipe(rename({ suffix: langcode }))
-                .pipe(dest('dist'))
-                .once('end', callback);
-        }));
+      src([...SOURCE_FILES, '!public/include/**/*.js', '!public/profile/**/*.js'], { read: false })
+        .pipe(tap(file => {
+          file.contents = browserify(file.path, { debug: isDevEnvironment })
+            .bundle();
+        }))
+        .pipe(buffer())
+        .pipe(translate(file.path))
+        .pipe(gulpIf(isDevEnvironment, sourcemaps.init({ loadMaps: true })))
+        .pipe(minify({
+          ext: {
+            src: '.src.js',
+            min: '.js'
+          }
+        }))
+        .pipe(gulpIf(isDevEnvironment, sourcemaps.write()))
+        .pipe(rename({ suffix: langcode }))
+        .pipe(dest('dist'))
+        .once('end', callback);
+    }));
 }
 
 exports.html = html;
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require('express');
 
 const app = express();
 const hbs = create({
-    partialsDir: path.join(__dirname, 'views', 'partials'),
+  partialsDir: path.join(__dirname, 'views', 'partials')
 });
 const port = 3000;
 
@@ -20,218 +20,221 @@ app.set('views', path.join(__dirname, 'views'));
 const filesInDist = listFiles(path.join('dist')).map(file => file.replace('dist/', ''));
 
 const handebarsHelpers = (poFile) => ({
-    i18n: (str, ...args) => i18n(str, poFile, args),
-    isoTime: (time) => new Date(time).toISOString(),
-    default: (value, defaultValue) => value || defaultValue,
-    eq: (v1, v2) => v1 === v2,
-    ne: (v1, v2) => v1 !== v2,
-    lt: (v1, v2) => v1 < v2,
-    gt: (v1, v2) => v1 > v2,
-    lte: (v1, v2) => v1 <= v2,
-    gte: (v1, v2) => v1 >= v2,
-    and() {
-        return Array.prototype.every.call(arguments, Boolean);
-    },
-    or() {
-        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
-    }
+  i18n: (str, ...args) => i18n(str, poFile, args),
+  isoTime: (time) => new Date(time).toISOString(),
+  default: (value, defaultValue) => value || defaultValue,
+  eq: (v1, v2) => v1 === v2,
+  ne: (v1, v2) => v1 !== v2,
+  lt: (v1, v2) => v1 < v2,
+  gt: (v1, v2) => v1 > v2,
+  lte: (v1, v2) => v1 <= v2,
+  gte: (v1, v2) => v1 >= v2,
+  and() {
+    return Array.prototype.every.call(arguments, Boolean);
+  },
+  or() {
+    return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+  }
 });
 
 const proxy = createProxyMiddleware({
-    target: 'http://localhost:4567',
-    changeOrigin: true,
-    selfHandleResponse: true,
-    onProxyRes: async function (proxyRes, req, res) {
-        if (proxyRes.statusCode >= 400) {
-            await sendErrorPage(res, proxyRes.statusCode);
-            return;
-        }
-        res.status(proxyRes.statusCode);
-        res.set(proxyRes.headers);
-        proxyRes.pipe(res);
-    },
+  target: 'http://localhost:4567',
+  changeOrigin: true,
+  selfHandleResponse: true,
+  onProxyRes: async function (proxyRes, req, res) {
+    if (proxyRes.statusCode >= 400) {
+      await sendErrorPage(req, res, proxyRes.statusCode);
+      return;
+    }
+    res.status(proxyRes.statusCode);
+    res.set(proxyRes.headers);
+    proxyRes.pipe(res);
+  }
 });
 
 const wsProxy = createProxyMiddleware({
-    target: 'ws://localhost:4567/ws',
-    changeOrigin: true,
-    ws: true,
-    onProxyReq: function (proxyReq, req, res) {
-        res.send();
-    }
+  target: 'ws://localhost:4567/ws',
+  changeOrigin: true,
+  ws: true,
+  onProxyReq: function (proxyReq, req, res) {
+    res.send();
+  }
 });
 
 const loadPO = (path) => new Promise((resolve, reject) => {
-    PO.load(path, (error, pofile) => {
-        if (error) {
-            reject(error);
-        } else {
-            resolve(pofile);
-        }
-    });
+  PO.load(path, (error, pofile) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve(pofile);
+    }
+  });
 });
 
 function i18n(str, poFile, args) {
-    const item = poFile.items.find(i => i.msgid === str);
-    if (!item) {
-        console.warn(`No translation found for ${str}`);
-    }
-    str = item && item.msgstr[0] || str;
-    for (let i = 0; i < args.length; i++) {
-        str = str.replace(`{${i}}`, args[i]);
-    }
-    return str;
+  const item = poFile.items.find(i => i.msgid === str);
+  if (!item) {
+    console.warn(`No translation found for ${str}`);
+  }
+  str = (item && item.msgstr[0]) || str;
+  for (let i = 0; i < args.length; i++) {
+    str = str.replace(`{${i}}`, args[i]);
+  }
+  return str;
 }
 
 app.use('/', async (req, res, next) => {
-    const fileName = req.path.slice(1);
-    const filePath = path.join(__dirname, 'dist', fileName);
-    const langCode = req.cookies['pxls-accept-language-override'] || 'en';
-    let poFile;
-    try {
-        poFile = await loadPO(path.join(__dirname, 'po', `Localization${langCode === 'en' ? '' : '_' + langCode}.po`));
-    } catch (e) {
-        console.error(e);
-        res.status(500).send('Internal Server Error');
-        return;
-    }
+  const fileName = req.path.slice(1);
+  const filePath = path.join(__dirname, 'dist', fileName);
+  const langCode = req.cookies['pxls-accept-language-override'] || 'en';
+  let poFile;
+  try {
+    poFile = await loadPO(path.join(__dirname, 'po', `Localization${langCode === 'en' ? '' : '_' + langCode}.po`));
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Internal Server Error');
+    return;
+  }
 
-    const proxyFetch = (url, options = {}) => fetch(url, {
-        headers: req.headers,
-        ...options,
+  const proxyFetch = (url, options = {}) => fetch(url, {
+    headers: req.headers,
+    ...options
+  });
+
+  if (fileName === '') {
+    res.render('index', {
+      title: 'pxls.space',
+      lang: langCode,
+      head: '',
+      scriptLang: langCode === 'en' ? '' : '_' + langCode,
+
+      helpers: {
+        i18n: (str, ...args) => i18n(str, poFile, args)
+      }
     });
-
-    if (fileName === '') {
-        res.render('index', {
-            title: 'pxls.space',
-            lang: langCode,
-            head: '',
-            scriptLang: langCode === 'en' ? '' : '_' + langCode,
-
-            helpers: {
-                i18n: (str, ...args) => i18n(str, poFile, args),
-            },
-        });
-    } else if (/^profile$|profile\/(?!js|css)/.test(fileName)) {
-        // get profile data
-        let profileName = fileName.split('/')[1] || '';
-        if (profileName !== '') profileName = '?username=' + profileName;
-        const dataRes = await proxyFetch('http://localhost:4567/api/v1/profile' + profileName);
-        const data = await dataRes.json();
-        if (data.details && data.details === 'USER_NOT_FOUND') {
-            await sendErrorPage(req, res, 404);
-            return;
-        }
-        console.log(data);
-        res.render('profile', {
-            profile_of: {
-                name: data.username,
-                displayedFactionId: data.displayedFactionId,
-                signupTime: data.signupTime,
-                signupTimeFormatted: new Date(+data.signupTime).toLocaleString(),
-                allTimePixelCount: data.pixelCountAllTime,
-                pixelCount: data.pixelCount,
-                discordName: data.discordName,
-                getRolesString: data.roleNames,
-                isBanned: data.isBanned,
-                isPermaBanned: data.isPermaBanned,
-                getBanExpiryTime: data.banExpiry,
-                getBanExpiryTimeFormatted: new Date(+data.banExpiry).toLocaleString(),
-                isChatbanned: data.isChatBanned,
-                isPermaChatbanned: data.isPermaChatBanned,
-                getChatBanExpiryTime: data.chatBanExpiry,
-                getChatBanExpiryTimeFormatted: new Date(+data.chatBanExpiry).toLocaleString(),
-                isFactionRestricted: data.isFactionRestricted,
-            },
-            route_root: '/' + fileName,
-            username: data.username,
-            requested_self: true,
-            requesting_user: {
-                name: 'Vanilla'
-            },
-            palette: 'FFFFFF,C2CBD4,858D98,4B4F58,22272D,000000,38271D,6C422C,BC7541,FFB27F,FFD6BF,FEB2D9,F854CF,C785F3,9C29BC,562972,1E1E5B,153FA2,1C95DF,A0E8FF,17A8A3,226677,094C45,278242,43C91E,B7F954,FFFFAF,FAE70F,FEA815,EA5B15,5A0400,990700,D81515,FF635E',
-            max_faction_tag_length: 5,
-            max_faction_name_length: 20,
-            canvas_reports_open_count: 0,
-            canvas_reports_length: 0,
-            chat_reports_open_count: 0,
-            chat_reports_length: 0,
-            snip_mode: false,
-            reportedName: 'REPORTEDNAME', // -snip- if snip_mode
-            canvas_reports: [
-                {
-                    closed: false,
-                    time: 0,
-                    message: 'REPORTMESSAGE',
-                }
-            ],
-            chat_reports: [
-                {
-                    closed: false,
-                    time: 0,
-                    report_message: 'REPORTMESSAGE',
-                }
-            ],
-            factions: data.factions,
-            keys: [
-                {
-                    key: 'KEY',
-                    value: 'VALUE',
-                }
-            ],
-
-            helpers: {
-                factionById: (id) => data.factions.find(f => f.id === id),
-
-                ...handebarsHelpers(poFile),
-            }
-        });
-    } else if (filesInDist.includes(fileName)) {
-        res.sendFile(filePath);
-    } else if (req.path.startsWith('/ws')) {
-        wsProxy(req, res, next);
-    } else {
-        proxy(req, res, next);
+  } else if (/^profile$|profile\/(?!js|css)/.test(fileName)) {
+    // get profile data
+    let profileName = fileName.split('/')[1] || '';
+    if (profileName !== '') profileName = '?username=' + profileName;
+    const dataRes = await proxyFetch('http://localhost:4567/api/v1/profile' + profileName);
+    if (dataRes.status >= 400) {
+      await sendErrorPage(req, res, dataRes.status);
+      return;
     }
+    const data = await dataRes.json();
+    if (data.details && data.details === 'USER_NOT_FOUND') {
+      await sendErrorPage(req, res, 404);
+      return;
+    }
+    res.render('profile', {
+      profile_of: {
+        name: data.username,
+        displayedFactionId: data.displayedFactionId,
+        signupTime: data.signupTime,
+        signupTimeFormatted: new Date(+data.signupTime).toLocaleString(),
+        allTimePixelCount: data.pixelCountAllTime,
+        pixelCount: data.pixelCount,
+        discordName: data.discordName,
+        getRolesString: data.roleNames,
+        isBanned: data.isBanned,
+        isPermaBanned: data.isPermaBanned,
+        getBanExpiryTime: data.banExpiry,
+        getBanExpiryTimeFormatted: new Date(+data.banExpiry).toLocaleString(),
+        isChatbanned: data.isChatBanned,
+        isPermaChatbanned: data.isPermaChatBanned,
+        getChatBanExpiryTime: data.chatBanExpiry,
+        getChatBanExpiryTimeFormatted: new Date(+data.chatBanExpiry).toLocaleString(),
+        isFactionRestricted: data.isFactionRestricted
+      },
+      route_root: '/' + fileName,
+      username: data.username,
+      requested_self: true,
+      requesting_user: {
+        name: 'Vanilla'
+      },
+      palette: 'FFFFFF,C2CBD4,858D98,4B4F58,22272D,000000,38271D,6C422C,BC7541,FFB27F,FFD6BF,FEB2D9,F854CF,C785F3,9C29BC,562972,1E1E5B,153FA2,1C95DF,A0E8FF,17A8A3,226677,094C45,278242,43C91E,B7F954,FFFFAF,FAE70F,FEA815,EA5B15,5A0400,990700,D81515,FF635E',
+      max_faction_tag_length: 5,
+      max_faction_name_length: 20,
+      canvas_reports_open_count: 0,
+      canvas_reports_length: 0,
+      chat_reports_open_count: 0,
+      chat_reports_length: 0,
+      snip_mode: false,
+      reportedName: 'REPORTEDNAME', // -snip- if snip_mode
+      canvas_reports: [
+        {
+          closed: false,
+          time: 0,
+          message: 'REPORTMESSAGE'
+        }
+      ],
+      chat_reports: [
+        {
+          closed: false,
+          time: 0,
+          report_message: 'REPORTMESSAGE'
+        }
+      ],
+      factions: data.factions,
+      keys: [
+        {
+          key: 'KEY',
+          value: 'VALUE'
+        }
+      ],
+
+      helpers: {
+        factionById: (id) => data.factions.find(f => f.id === id),
+
+        ...handebarsHelpers(poFile)
+      }
+    });
+  } else if (filesInDist.includes(fileName)) {
+    res.sendFile(filePath);
+  } else if (req.path.startsWith('/ws')) {
+    wsProxy(req, res, next);
+  } else {
+    proxy(req, res, next);
+  }
 });
 
 async function sendErrorPage(req, res, code) {
-    const langCode = req.cookies['pxls-accept-language-override'] || 'en';
-    let poFile;
-    try {
-        poFile = await loadPO(path.join(__dirname, 'po', `Localization${langCode === 'en' ? '' : '_' + langCode}.po`));
-    } catch (e) {
-        console.error(e);
-        res.status(500).send('Internal Server Error');
-        return;
-    }
+  const langCode = req.cookies['pxls-accept-language-override'] || 'en';
+  let poFile;
+  try {
+    poFile = await loadPO(path.join(__dirname, 'po', `Localization${langCode === 'en' ? '' : '_' + langCode}.po`));
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Internal Server Error');
+    return;
+  }
 
-    res.render('404', {
-        palette: 'FFFFFF,C2CBD4,858D98,4B4F58,22272D,000000,38271D,6C422C,BC7541,FFB27F,FFD6BF,FEB2D9,F854CF,C785F3,9C29BC,562972,1E1E5B,153FA2,1C95DF,A0E8FF,17A8A3,226677,094C45,278242,43C91E,B7F954,FFFFAF,FAE70F,FEA815,EA5B15,5A0400,990700,D81515,FF635E',
-        max_faction_tag_length: 5,
-        max_faction_name_length: 20,
-        requesting_user: {
-            name: 'Vanilla'
-        },
-        err: code,
+  res.render('404', {
+    palette: 'FFFFFF,C2CBD4,858D98,4B4F58,22272D,000000,38271D,6C422C,BC7541,FFB27F,FFD6BF,FEB2D9,F854CF,C785F3,9C29BC,562972,1E1E5B,153FA2,1C95DF,A0E8FF,17A8A3,226677,094C45,278242,43C91E,B7F954,FFFFAF,FAE70F,FEA815,EA5B15,5A0400,990700,D81515,FF635E',
+    max_faction_tag_length: 5,
+    max_faction_name_length: 20,
+    requesting_user: {
+      name: 'Vanilla'
+    },
+    err: code,
 
-        helpers: handebarsHelpers(poFile),
-    });
+    helpers: handebarsHelpers(poFile)
+  });
 }
 
 function listFiles(dir, fileList = []) {
-    const files = fs.readdirSync(dir);
+  const files = fs.readdirSync(dir);
 
-    files.forEach(file => {
-        const filePath = path.join(dir, file);
-        if (fs.statSync(filePath).isDirectory()) {
-            listFiles(filePath, fileList);
-        } else {
-            fileList.push(filePath.replace(__dirname, '').replaceAll('\\', '/'));
-        }
-    });
+  files.forEach(file => {
+    const filePath = path.join(dir, file);
+    if (fs.statSync(filePath).isDirectory()) {
+      listFiles(filePath, fileList);
+    } else {
+      fileList.push(filePath.replace(__dirname, '').replaceAll('\\', '/'));
+    }
+  });
 
-    return fileList;
+  return fileList;
 }
 
-app.listen(port, () => console.log(`Listening at http://localhost:3000/`));
+app.listen(port, () => console.info('Listening at http://localhost:3000/'));
